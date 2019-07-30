7a93466dca9df34d3d6b307085a1594862abe730
cafehaine <kilian.guillaume@gmail.com>
2019-07-30 13:48:38 +0200

    Make it a library

--- luwee.lua
+++ luwee.lua
@@ -1,60 +1,41 @@
-local decode = require("luwee.decode")
-
-local server_address =""
-local server_port = 0
-local server_password = ""
-
-local args = {"password="..server_password, "compression=off"}
+local m = {}
 
 local socket = require("socket")
 local ssl = require("ssl")
 
--- TLS/SSL client parameters (omitted)
-local params = {
-	mode = "client",
-	protocol = "any",
-}
+local decode = require("luwee.decode")
 
-local conn, err = socket.tcp()
-if not conn then error(err) end
-local success, err = conn:connect(server_address, server_port)
-if not success then error(err) end
+local connection = nil
 
--- TLS/SSL initialization
-conn = ssl.wrap(conn, params)
-if not conn then error(err) end
-conn:dohandshake()
-for i,cert in ipairs(conn:getpeerchain()) do
---	print("sha1:", cert:digest())
---	print("issuer:", cert:issuer())
+function m.connect(server_address, server_port, server_ssl, server_password)
+	if not server_ssl then
+		error("Only ssl is suported for now.")
+	end
+	local conn, err = socket.tcp()
+	if not conn then error(err) end
+	local success, err = conn:connect(server_address, server_port)
+	if not success then error(err) end
+	local conn, err = ssl.wrap(conn, {mode="client", protocol="any"})
+	if not conn then error(err) end
+	--TODO find a way to ask the user if they trust the certificate
+	conn:dohandshake()
+	conn:send(("init password=%s,compression=off\n"):format(server_password))
+	connection = conn
 end
 
-conn:send("init "..table.concat(args,",").."\n")
--- no return data ?
+function m.check_input()
+	if connection == nil then return end
+	connection:settimeout(0.1)
+	local len_bytes, err = connection:receive(4)
+	if not len_bytes and err == "closed" then
+		error("connection closed")
+	elseif not len_bytes then
+		return -- timeout
+	end
+	local len = decode.length(len_bytes) - 4
+	connection:settimeout(nil)
+	local answer = conn:receive(len)
+	decode.message(answer)
+end
 
---conn:send("info version\n")
---local len = decode.length(conn:receive(4)) - 4
---local answer = conn:receive(len)
---decode.message(answer)
-
---conn:send("ping 17\n")
---local len = decode.length(conn:receive(4)) - 4
---local answer = conn:receive(len)
---decode.message(answer)
-
---conn:send("infolist buffer\n")
---local len = decode.length(conn:receive(4)) - 4
---local answer = conn:receive(len)
---decode.message(answer)
-
-conn:send("test\n")
-local len = decode.length(conn:receive(4)) - 4
-local answer = conn:receive(len)
-decode.message(answer)
-
---conn:send("nicklist\n")
---local len = decode.length(conn:receive(4)) - 4
---local answer = conn:receive(len)
---decode.message(answer)
-conn:send("quit\n")
-conn:close()
+return m
